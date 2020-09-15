30b9a8e322a3dae10da78194053b0f4fa0c8e536
Andrey Surovyy <turp92@mail.ru>
2020-09-15 16:02:51 +0700

    15.09.20

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    //[SerializeField]
+    //private WindowsController.WindowsType windowType = default;
+
+    public void OpenWindow()
+    {
+        WindowsController.Instance.OpenWindow();
+    }
+
+    public void CloseWindow()
+    {
+        WindowsController.Instance.CloseWindow();
+    }
+
+}
---
+++ Assets/Scripts/WindowsController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsController : MonoBehaviour
+{
+    public static WindowsController Instance;
+
+    [SerializeField]
+    private Canvas canvas = null;
+
+    [SerializeField]
+    private GameObject windowPref = null;
+
+    public GameObject curOpenWindow = null;
+
+    Animator anim = null;
+
+    //public enum WindowsType
+    //{
+    //    START_GAME,
+    //    SETTINGS,
+    //    STORE,
+    //    EXIT,
+    //    CLOSE_WINDOW
+    //}
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void OpenWindow()
+    {
+        if (curOpenWindow != null)
+        {
+            Destroy(curOpenWindow);
+        }
+
+        curOpenWindow = Instantiate(windowPref, canvas.transform);
+        anim = curOpenWindow.GetComponent<Animator>();
+    }
+
+    public void CloseWindow()
+    {
+        if (curOpenWindow != null)
+        {
+            anim.SetTrigger("Close");
+        }
+    }
+}
